785c4942078ead666441b33fc02c2f977476ec39
José González <jgg032899@gmail.com>
2018-12-01 14:19:16 -0400

    Se conectan ambos nodos (ya que el grafo no es dirigido)

--- src/tsp/tsp.go
+++ src/tsp/tsp.go
@@ -2,156 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 	"time"
 )
 
-// Implementación del Stack
-type (
-	Stack struct {
-		top    *stackNode
-		length int
-	}
-	stackNode struct {
-		value interface{}
-		prev  *stackNode
-	}
-)
-
-func NewStack() *Stack {
-	return &Stack{nil, 0}
-}
-
-func (this *Stack) Len() int {
-	return this.length
-}
-
-func (this *Stack) Peek() interface{} {
-	if this.length == 0 {
-		return nil
-	}
-	return this.top.value
-}
-
-func (this *Stack) Pop() interface{} {
-	if this.length == 0 {
-		return nil
-	}
-
-	n := this.top
-	this.top = n.prev
-	this.length--
-	return n.value
-}
-
-func (this *Stack) Push(value interface{}) {
-	n := &stackNode{value, this.top}
-	this.top = n
-	this.length++
-}
-
-//
-
 type Node struct {
 	label     int
 	visited   bool
-	neighbors map[int][]*Node
+	neighbors map[int]*Edge
+	position  Position
 }
 
 type Graph struct {
 	nodes map[int]*Node
 }
 
-func (g *Graph) dfs(node *Node) int {
-	node.visited = true
-
-	count := 0
-
-	if len(node.neighbors) != 0 {
-		for _, v := range node.neighbors[node.label] {
-			if v.visited == false {
-				count += g.dfs(v)
-			}
-		}
-	}
-
-	return count + 1
+type Position struct {
+	posX float64
+	posY float64
 }
 
-// Se visita un nodo y se comprueban todos los nodos a los que se puede llegar desde aquí, luego de terminar de un nodo,
-// se guarda en el stack
-
-func (g *Graph) fillOrder(node *Node, stack *Stack) {
-	node.visited = true
-
-	if len(node.neighbors) != 0 {
-		for _, v := range node.neighbors[node.label] {
-			if v.visited == false {
-				g.fillOrder(v, stack)
-			}
-		}
-	}
-
-	stack.Push(node.label)
-}
-
-// Función que se encarga de procesar e imprimir los SCC resultantes
-
-func (g *Graph) printSCC(bytesRead []byte) {
-	start := time.Now()
-
-	stack := NewStack()
-
-	var n []int
-	count := 0
-
-	// Se colocan los nodos en el stack
-	for label := range g.nodes {
-		if g.nodes[label].visited == false {
-			g.fillOrder(g.nodes[label], stack)
-		}
-	}
-
-	gr := CreateGraph(bytesRead, true)
-
-	for stack.Len() > 0 {
-		v := (stack.Pop()).(int)
-
-		if gr.nodes[v].visited == false {
-			count = gr.dfs(gr.nodes[v])
-			n = append(n, count)
-		}
-	}
-
-	sort.Sort(sort.Reverse(sort.IntSlice(n)))
-
-	fmt.Print("Los 5 SCC más grandes: ")
-
-	for i := 0; i < 5; i++ {
-		if i < len(n) {
-			if i != 4 {
-				fmt.Print(n[i], ", ")
-			} else {
-				fmt.Print(n[i])
-			}
-		} else {
-			if i != 4 {
-				fmt.Print(0, ", ")
-			} else {
-				fmt.Print(0)
-			}
-		}
-	}
-
-	fmt.Println()
-
-	var elapsed time.Duration
-	elapsed = time.Since(start)
-
-	fmt.Printf("SCC took %s\n", elapsed)
+type Edge struct {
+	node   *Node
+	weight float64
 }
 
 func ReadFile(name string) []byte {
@@ -201,12 +77,29 @@ func (g *Graph) GetNode(label int) *Node {
 func CreateNode(label int) *Node {
 	n := new(Node)
 	n.label = label
-	n.neighbors = make(map[int][]*Node)
+	n.neighbors = make(map[int]*Edge)
 	return n
 }
 
 func (g *Graph) AddEdge(nini *Node, nfin *Node) {
-	nini.neighbors[nini.label] = append(nini.neighbors[nini.label], nfin)
+	//nini.neighbors[nini.label] = append(nini.neighbors[nini.label], nfin)
+}
+
+func CreateEdge(node *Node, weight float64) *Edge {
+	e := new(Edge)
+	e.node = node
+	e.weight = weight
+
+	return e
+}
+
+func SetDistance(nini *Node, nfin *Node) {
+	x := math.Pow(nfin.position.posX-nini.position.posX, 2)
+	y := math.Pow(nfin.position.posY-nini.position.posY, 2)
+	z := math.Sqrt(x + y)
+
+	nini.neighbors[nfin.label] = CreateEdge(nfin, z)
+	nfin.neighbors[nini.label] = CreateEdge(nini, z)
 }
 
 func CreateGraph(bytesRead []byte, rev bool) *Graph {
@@ -284,7 +177,5 @@ func main() {
 	if gr == nil {
 		return
 	}
-
-	gr.printSCC(bytesRead)
 
 }
